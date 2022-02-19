4f3b4871b5cd65604f3e54bcc58ad3fc8e677f94
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-19 23:30:33 +0300

    Basic Lazy Column items

--- app/src/main/java/com/metehanbolat/lazycolumnitemclickcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/lazycolumnitemclickcompose/MainActivity.kt
@@ -3,6 +3,19 @@ package com.metehanbolat.lazycolumnitemclickcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.lazycolumnitemclickcompose.ui.theme.LazyColumnItemClickComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +23,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LazyColumnItemClickComposeTheme {
+                DynamicList()
+            }
+        }
+    }
+}
 
+@Composable
+fun DynamicList() {
+    val countryList = remember { mutableStateListOf("Türkiye", "İtalya", "Almanya", "Japonya") }
+
+    LazyColumn {
+        items(count = countryList.count()) {
+            val country = countryList[it]
+
+            Card(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+            ) {
+                Row {
+                    Row(
+                        modifier = Modifier.padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = country,
+                            modifier = Modifier.padding(5.dp)
+                        )
+                    }
+                }
             }
         }
     }
