f74506575b6a1c899240fb58803690398fba7f14
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-19 23:40:58 +0300

    Clickable widgets

--- app/src/main/java/com/metehanbolat/lazycolumnitemclickcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/lazycolumnitemclickcompose/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.metehanbolat.lazycolumnitemclickcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,15 +45,26 @@ fun DynamicList() {
                     .padding(5.dp)
                     .fillMaxWidth()
             ) {
-                Row {
+                Row(
+                    modifier = Modifier.clickable {
+                        Log.d("List", "$country row selected!")
+                    }
+                ) {
                     Row(
-                        modifier = Modifier.padding(10.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                        modifier = Modifier.padding(10.dp).fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Text(
                             text = country,
                             modifier = Modifier.padding(5.dp)
                         )
+
+                        OutlinedButton(onClick = {
+                            Log.d("List", "$country button selected!")
+                        }) {
+                            Text(text = "Choose")
+                        }
                     }
                 }
             }
